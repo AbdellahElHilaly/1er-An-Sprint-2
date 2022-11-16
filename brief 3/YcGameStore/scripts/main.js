58a73a402df75b69c850d15f54fd9da0a316c423
abdellah el hilaly <abdellah.elhilaly.96@gmail.com>
2022-11-16 09:17:30 +0100

    first user story ( sing in & sing up ) : maket : done, front end : done, bach end : start .

---
+++ brief 3/YcGameStore/scripts/main.js
@@ -0,0 +1,29 @@
+nav_list_home.addEventListener("click" , (params) => {
+    document.getElementById('splash_home_id').style.display="block";
+    document.getElementById('forme_sing_id').style.display="none";
+})
+
+nav_list_sing_in.addEventListener("click" , (params) => {
+    document.getElementById('splash_home_id').style.display="none";
+    document.getElementById('forme_sing_id').style.display="block";
+
+    forme_sing_label_id.innerHTML="Sing in";
+
+    document.getElementById('forme_sing_name_id').style.display="flex";
+    document.getElementById('forme_sing_conf_password_id').style.display="flex";
+})
+
+
+nav_list_sing_up.addEventListener("click" , (params) => {
+    document.getElementById('splash_home_id').style.display="none";
+    document.getElementById('forme_sing_id').style.display="block";
+
+    forme_sing_label_id.innerHTML="Sing up";
+
+    document.getElementById('forme_sing_name_id').style.display="none";
+    document.getElementById('forme_sing_conf_password_id').style.display="none";
+    
+    
+    
+})
+
